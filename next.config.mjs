6f01e512dc8df34411fd3729bd11d03e1fb843c7
irufano <irfanhidayatms@gmail.com>
2024-08-24 00:03:22 +0700

    coba

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/irufano.github.io/" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "https://irufano.github.io/" : "",
   images: {
     unoptimized: true,
   },
