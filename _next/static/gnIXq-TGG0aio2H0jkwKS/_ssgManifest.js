5eb030898bc04a52c5a9b3a2f799b3750810d6e4
irufano <41888110+irufano@users.noreply.github.com>
2024-08-26 07:21:31 +0000

    Deploying to public from @ irufano/irufano.github.io@3c34009b559e72e43ea8124e319a4924494eee61 🚀

---
+++ _next/static/gnIXq-TGG0aio2H0jkwKS/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Finsight","\u002Finsight\u002F[page]","\u002Finsight\u002Fposts\u002F[slug]","\u002Finsight\u002Fsearch"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
