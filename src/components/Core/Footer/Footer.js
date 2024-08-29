b1aeefb941908cd9f55232b84611d202b53ba812
irufano <irfanhidayatms@gmail.com>
2024-08-29 19:05:10 +0700

    fix footer

--- src/components/Core/Footer/Footer.js
+++ src/components/Core/Footer/Footer.js
@@ -2,19 +2,36 @@ import Image from "next/image";
 import Logo from "../../../assets/irufano-square-logo.svg";
 import IrufanoLogo from "@/components/Logo/IrufanoLogo";
 import FeatherIcon from "feather-icons-react";
+import Link from "next/link";
 
 const Footer = () => {
   const d = new Date();
   let year = d.getFullYear();
   return (
-    <footer className="container mx-auto p-4 py-16 text-center   ">
-      <div className="flex flex-col md:flex-row md:items-end md:justify-between justify-center  items-center  space-y-1">
-        <div>
-          <IrufanoLogo />
-        </div>
-        <Image src={Logo} className="hidden md:inline-block w-ful w-4 h-auto" />
-        <div className="flex items-center ">
-          <h3 className="text-sm text-gray-500">irufano.github.io © {year}</h3>
+    <footer className="py-16 text-center bg-surface dark:bg-surface-dark">
+      <div className="container mx-auto p-4">
+        <div className="flex flex-col md:flex-row md:items-end md:justify-between justify-center  items-center  space-y-1">
+          <div>
+            <Link href={"https://irufano.com"}>
+              <IrufanoLogo />
+            </Link>
+          </div>
+          <Image
+            src={Logo}
+            alt="-"
+            className="hidden md:inline-block w-ful w-4 h-auto"
+          />
+          <div className="flex items-center ">
+            <Link href={"/"}>
+              <h3 className="text-sm text-gray-500">
+                irufano.github.io{" "}
+                <span className="font-light text-gray-400 dark:text-gray-600">
+                  |
+                </span>{" "}
+                {year}
+              </h3>
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
