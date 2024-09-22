4e0dc9a17f862bcc4bb5d2c71472604d632bcd69
irufano <irfanhidayatms@gmail.com>
2024-09-22 16:53:36 +0700

    add toolst next seo

---
+++ tools-next-seo.config.js
@@ -0,0 +1,35 @@
+export default {
+  title: "Irufano Tools - Tech Knowledge Docs",
+  description: "Insights, ideas, stories, tutorials and technical documentations about technology, software development and other IT matters.",
+  canonical: "https://irufano.github.io/tools",
+  host: "https://irufano.github.io",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://irufano.github.io/tools",
+    site_name: "Irufano Insight",
+    title: "Irufano Insight",
+    description: "Insights, ideas, stories, tutorials and technical documentations about technology, software development and other IT matters.",
+    images: [
+      {
+        url: "https://irufano.github.io/og-irufano-insight-01.png",
+        width: 800,
+        height: 600,
+        alt: "Irufano Tools",
+        type: "image/png",
+      },
+      {
+        url: "https://irufano.github.io/og-irufano-insight-02.png",
+        width: 900,
+        height: 800,
+        alt: "Irufano Tools",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
